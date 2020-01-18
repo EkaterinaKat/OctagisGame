14f7cd03e79916754de7055a82b9545fef36157b
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2020-01-18 23:59:10 +0300

    Добавила GameActivity

--- app/src/main/java/com/octagisgame/MainActivity.java
+++ app/src/main/java/com/octagisgame/MainActivity.java
@@ -2,6 +2,7 @@ package com.octagisgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             startBtn.setImageResource(R.drawable.play_pressed);
+            Intent intent = new Intent(MainActivity.this,GameActivity.class);
+            startActivity(intent);
         }
     };
 }
