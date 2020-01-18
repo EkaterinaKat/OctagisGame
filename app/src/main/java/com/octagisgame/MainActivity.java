38dd8330aa7447ef9386010ac5d77216680bb260
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2020-01-18 22:13:41 +0300

    Оформила стартовую активность

--- app/src/main/java/com/octagisgame/MainActivity.java
+++ app/src/main/java/com/octagisgame/MainActivity.java
@@ -3,12 +3,25 @@ package com.octagisgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.start_button);
+        startBtn.setOnClickListener(startGame);
     }
+
+    private final View.OnClickListener startGame = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startBtn.setImageResource(R.drawable.play_pressed);
+        }
+    };
 }
