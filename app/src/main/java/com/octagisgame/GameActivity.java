19a151e33f0c32cf866535db8c0598d130865daa
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2020-01-22 00:52:19 +0300

    Установила горизонтальную ориентацию для обеих активностей и изменила источник изображения для второй активности

--- app/src/main/java/com/octagisgame/GameActivity.java
+++ app/src/main/java/com/octagisgame/GameActivity.java
@@ -2,13 +2,29 @@ package com.octagisgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(new DrawView(this));
+    }
+
+    class DrawView extends View {
+
+        public DrawView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawColor(Color.GREEN);
+        }
     }
 }
